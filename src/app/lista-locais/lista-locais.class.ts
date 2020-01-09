ddbdf72202cb4b9061bd02af6b2da800c30fd4df
Gabriel <gabrielfvs91@gmail.com>
2020-01-09 08:16:06 -0300

    Commiting files lista-locais, locais

---
+++ src/app/lista-locais/lista-locais.class.ts
@@ -0,0 +1,8 @@
+import { Local } from './locais';
+
+export const LOCAIS: Local[] = [
+    {nome:'Argentina', id:0,preco:1200},
+    {nome:'Brazil', id:1,preco:1225},
+    {nome:'Uruguai', id:2,preco:1230},
+    {nome:'China', id:3,preco:1232},
+]
